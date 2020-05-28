1ff23749280a39d2e34cf01ab2323908c0e4d800
wjy5446 <wjy5446@gmail.com>
2020-05-28 23:14:35 +0900

    feat) add get, add babel

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-const express = require("express");
+import express from "express";
 const app = express();
 
 const PORT = 4000;
 
-function handleListening() {
+const handleListening = () =>
   console.log(`Listening on: http://localhost:${PORT}`);
-}
 
+const handleHome = (req, res) => res.send("Hello from home!!");
+
+const handleProfile = (req, res) => res.send("You are on my profile");
+
+app.get("/", handleHome);
+app.get("/profile", handleProfile);
 app.listen(PORT, handleListening);
