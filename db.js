eda01bff0745fe895959469118c09203c0ee40e7
wjy5446 <wjy5446@gmail.com>
2020-05-30 15:25:37 +0900

    feat) add form template

---
+++ db.js
@@ -0,0 +1,50 @@
+export const videos = [
+  {
+    id: 123423,
+    title: "Video awesome",
+    description: "This is something I love",
+    views: 24,
+    videoFile: "https://archive.org/details/BigBuckBunny/_124",
+    creator: {
+      id: 1234324,
+      name: "Nicolas",
+      email: "nico@las.com",
+    },
+  },
+  {
+    id: 22222,
+    title: "super",
+    description: "This is something I love",
+    views: 24,
+    videoFile: "https://archive.org/details/BigBuckBunny/_124",
+    creator: {
+      id: 1234324,
+      name: "Nicolas",
+      email: "nico@las.com",
+    },
+  },
+  {
+    id: 3333,
+    title: "Video awesome3",
+    description: "This is something I love",
+    views: 24,
+    videoFile: "https://archive.org/details/BigBuckBunny/_124",
+    creator: {
+      id: 1234324,
+      name: "Nicolas",
+      email: "nico@las.com",
+    },
+  },
+  {
+    id: 4444,
+    title: "Video awesome4",
+    description: "This is something I love",
+    views: 24,
+    videoFile: "https://archive.org/details/BigBuckBunny/_124",
+    creator: {
+      id: 1234324,
+      name: "Nicolas",
+      email: "nico@las.com",
+    },
+  },
+];
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -11,8 +11,8 @@ import {
 const videoRouter = express.Router();
 
 videoRouter.get(routes.upload, upload);
-videoRouter.get(routes.videoDetail, videoDetail);
 videoRouter.get(routes.editVideo, editVideo);
 videoRouter.get(routes.deleteVideo, deleteVideo);
+videoRouter.get(routes.videoDetail, videoDetail);
 
 export default videoRouter;
