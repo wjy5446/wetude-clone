40cb1fc2b16d8e031cc5fcba1a5285a1cb2e20c2
wjy5446 <wjy5446@gmail.com>
2020-05-30 16:56:43 +0900

    feat) modify router

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -13,6 +13,6 @@ const videoRouter = express.Router();
 videoRouter.get(routes.upload, upload);
 videoRouter.get(routes.editVideo, editVideo);
 videoRouter.get(routes.deleteVideo, deleteVideo);
-videoRouter.get(routes.videoDetail, videoDetail);
+videoRouter.get(routes.videoDetail(), videoDetail);
 
 export default videoRouter;
