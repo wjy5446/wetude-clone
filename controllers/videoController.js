35f4781a9f1a2213681e789326d0758aee53d73e
wjy5446 <wjy5446@gmail.com>
2020-06-07 00:10:53 +0900

    feat) add search video

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -3,18 +3,31 @@ import Video from "../models/Video";
 
 export const home = async (req, res) => {
   try {
-    const videos = await Video.find({});
+    const videos = await Video.find({}).sort({ _id: -1 });
     res.render("home", { pageTitle: "Home", videos });
   } catch (error) {
     console.log(error);
     res.render("home", { pageTitle: "Home", videos: [] });
   }
 };
-export const search = (req, res) => {
+export const search = async (req, res) => {
   const {
     query: { term: searchingBy },
   } = req;
-  res.render("search", { pageTitle: "Search", searchingBy, videos });
+
+  let videos = [];
+  try {
+    videos = await Video.find({
+      title: { $regex: searchingBy, $options: "i" },
+    });
+  } catch (error) {
+    console.log(error);
+  }
+  res.render("search", {
+    pageTitle: "Search",
+    searchingBy,
+    videos,
+  });
 };
 
 export const getUpload = (req, res) =>
